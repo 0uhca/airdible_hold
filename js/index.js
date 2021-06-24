ad583c1cd69a1ad81060130b88d80f476c6576a4
0uhca <georgeokangaonline@gmail.com>
2021-06-24 15:26:53 +0100

    Commit for resolving icons issues

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 const selectElement = (element) => document.querySelector(element);
+const navList = document.querySelector('.nav-list');
+const navToggle = document.querySelector('.toggle');
+const navLink = document.querySelector('.nav-link');
+
+navToggle.textContent = 'Menu'
 
 selectElement ('.hamburger').addEventListener('click', () => {
-    selectElement('.hamburger').classList.toggle('.active')
+    selectElement('.hamburger').classList.toggle('active');
+    navList.classList.toggle('active');
+    
+    // if (navList.classList.contains('active')) {
+    //     navToggle.textContent = 'Close Menu';
+    // } else {
+    //     navToggle.textContent = 'Open Menu';
+    // }
+
+    if (navList.classList.contains('active')) {
+        navToggle.style.color = '#000';
+    } else {
+        navToggle.style.color = '#9E9E9E';
+    }
+
 });
+
+
+
+
+
